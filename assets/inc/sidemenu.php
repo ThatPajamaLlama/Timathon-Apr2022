9b69c016564da56503505e17a7c243ecbf40b0d6
Nozzergirl <shannon_norris@hotmail.co.uk>
2022-04-08 18:41:18 +0100

    Active sidemenu choice

--- assets/inc/sidemenu.php
+++ assets/inc/sidemenu.php
@@ -2,17 +2,17 @@
     <h1 id="logo">To the fullest</h1>
     <nav class="sidemenu">
         <ul>
-            <li><a href="dash.php" class="active"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>
+            <li><a href="dash.php"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>
         </ul>
         <h2>Change Today</h2>
         <ul>
-            <li><a href="dash.php"><i class="fa fa-paint-brush" aria-hidden="true"></i>Style Generator</a></li>
-            <li><a href="dash.php"><i class="fa fa-thumbs-up" aria-hidden="true"></i>Activity Randomiser</a></li>
+            <li><a href="stylegenerator.php"><i class="fa fa-paint-brush" aria-hidden="true"></i>Style Generator</a></li>
+            <li><a href="activityrandomiser.php"><i class="fa fa-thumbs-up" aria-hidden="true"></i>Activity Randomiser</a></li>
         </ul>
         <h2>Change Everyday</h2>
         <ul>
-            <li><a href="dash.php"><i class="fa fa-thumb-tack" aria-hidden="true"></i>Vision Board</a></li>
-            <li><a href="dash.php"><i class="fa fa-list" aria-hidden="true"></i>Bucket List</a></li>
+            <li><a href="visionboard.php"><i class="fa fa-thumb-tack" aria-hidden="true"></i>Vision Board</a></li>
+            <li><a href="bucketlist.php"><i class="fa fa-list" aria-hidden="true"></i>Bucket List</a></li>
         </ul>
         <h2>Peer Support</h2>
         <ul>
@@ -24,3 +24,8 @@
         </ul>
     </nav>
 </header>
+
+<script>
+    var currentPage = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+    document.querySelector('nav a[href^="' + currentPage + '"]').classList.add('active');
+</script>
