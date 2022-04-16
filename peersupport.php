8daf14b174085a61ef97a1b77ed29384e8075839
Nozzergirl <shannon_norris@hotmail.co.uk>
2022-04-16 20:37:03 +0100

    New section on peer support

--- peersupport.php
+++ peersupport.php
@@ -10,17 +10,26 @@ include "assets/inc/user_access_control.php";
         <?php include "assets/inc/sidemenu.php";?>
         
         <main>
-            <section id="new-post">
-                <form onsubmit="return CreatePost(event, this);" method="post">
-                    <div class="flex-container">
-                        <textarea id="post" name="post" placeholder="How did you make today a little less ordinary?"></textarea>
-                        <button type="submit"><i class="fa fa-share-square-o" aria-hidden="true"></i>Share</button>
-                    </div>
-                </form>
-            </section>
-            <section id="posts">
-                <!-- Shows all posts from all users -->
-            </section>
+            <div class="flex-container">
+                <div id="social">
+                    <section id="new-post">
+                        <form onsubmit="return CreatePost(event, this);" method="post">
+                            <div class="flex-container">
+                                <textarea id="post" name="post" placeholder="How did you make today a little less ordinary?"></textarea>
+                                <button type="submit"><i class="fa fa-share-square-o" aria-hidden="true"></i>Share</button>
+                            </div>
+                        </form>
+                    </section>
+                    <section id="posts">
+                        <!-- Shows all posts from all users -->
+                    </section>
+                </div>
+                <div id="info">
+                    <h1>What is this?</h1>
+                    <p>This is a sharing area where you'll find like-minded people sharing what they're doing to make everyday a little less ordinary.</p>
+                    <p>We believe that <span class="site-name">to the fullest</span> is a community, which revolves around a shared mission. This sharing area is here so that the group can collectively inspire one another with ideas for how to enrich our daily lives.</p>
+                </div>
+            </div>
         </main>
 
     </body>
