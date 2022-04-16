a72388806de2cdd666af3b4f48d3a8ca37efee27
Nozzergirl <shannon_norris@hotmail.co.uk>
2022-04-16 22:01:59 +0100

    Mobile layouts

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -1,5 +1,6 @@
 <head>
     <link rel="stylesheet" type="text/css" href="assets/style.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="icon" href="assets/newicon.svg" type="image/svg">
     <title>To the Fullest</title>
 </head>
